e9eee9d4408fb17067fb38e97f67940d60a1e27a
Mr.Ye <Owen718@users.noreply.github.com>
2020-08-22 22:24:21 +0800

    v0.5

--- Underwater Image Enhancement Using Adaptive Retinal Mechanisms.py
+++ Underwater Image Enhancement Using Adaptive Retinal Mechanisms.py
@@ -8,16 +8,11 @@ def cv_show(name,img):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+
+
 def mat_mean(img,θ,N):  #计算各通道均值，根据θ来判断返回R通道最大值或平均值,目前暂定N为3
     global original_img
-    mean_kernel = np.array([
-        [1/9,1/9,1/9],
-        [1/9,1/9,1/9],
-        [1/9,1/9,1/9]
-    ])   #均值算子
-    #B_mean = cv2.filter2D(img[:,:,0],ddepth=-1,kernel=mean_kernel)  #B通道卷积计算
-    #G_mean = cv2.filter2D(img[:,:,1],ddepth=-1,kernel=mean_kernel)  #G通道卷积计算
-
+ 
     B_mean =cv2.boxFilter(img[:,:,0],ddepth=-1,ksize=(N,N),normalize=True)
     G_mean =cv2.boxFilter(img[:,:,1],ddepth=-1,ksize=(N,N),normalize=True)
 
@@ -32,28 +27,18 @@ def mat_mean(img,θ,N):  #计算各通道均值，根据θ来判断返回R通道
     R_mean = np.zeros((row,col),dtype = np.float64)
     for x in range(row-N):  #遍历行
         for y in range(col-N):  #遍历列
-            xy_zone = R[x:x+N,y:y+N] 
-            if xy_zone.max() > θ:  #若是最大值大于θ
-               ## for i in range(xy_zone.shape[0]):
-                   # for j in range(xy_zone.shape[1]):
-                   #     if xy_zone[i,j]<θ:
-                    #       xy_zone[i,j]=θ
-                #if  np.mean(xy_zone) < θ:
-                #    R_mean[x,y]=θ
-                #else:
-                #R_mean[x,y]=np.mean(xy_zone)
-                #R_mean[x:x+3,y:y+3]=cv2.boxFilter(xy_zone,ddepth=-1,ksize=(N,N),anchor=None,normalize=False)
-                #max_num +=1
+            xy_zone = R[x:x+N,y:y+N]
+            if xy_zone.max() < θ:  #若是最大值小于θ
                 R_mean[x,y]=xy_zone.max()
-                mean_num += 1
-            else:  #若是最大值小于θ
-                #R_mean[x,y]=np.mean(xy_zone)  #取均值
-                #R_mean[x:x+3,y:y+3]=cv2.boxFilter(xy_zone,ddepth=-1,ksize=(N,N),normalize=False)
-                #mean_num += 1
                 max_num += 1
-                R_mean[x,y]=np.mean(xy_zone)
+                xy_zone=[]
+            else:  #若是最大值小于θ
+                mean_num += 1
+                R_mean[x,y]=np.mean(xy_zone[xy_zone>θ])
                 #original_view = cv2.circle(original_view,center=(y,x),radius = 1,color = (255,255,255))#绘制区域
-                original_view[x,y,:] = [0,0,255]
+                original_view[x,y,:] = [255,0,0]
+                xy_zone=[]
+     
                 
     cv_show('original_view',original_view)
     print('max_num:'+str(max_num))
@@ -169,19 +154,19 @@ def getDarkChannel(img, blockSize):
 
 # 获取最小值矩阵
 # 获取BGR三个通道的最小值
-def getMinChannel(img):
+def getMinChannel(img):  #return np.float64 imgGray
     # 输入检查
     if len(img.shape) == 3 and img.shape[2] == 3:
         pass
     else:
         print("bad image shape, input must be color image")
         return None
-    imgGray = np.zeros((img.shape[0], img.shape[1]), dtype=np.uint8)
-    localMin = 255
+    imgGray = np.zeros((img.shape[0], img.shape[1]), dtype=np.float64)
+    localMin = 999999
 
     for i in range(0, img.shape[0]):
         for j in range(0, img.shape[1]):
-            localMin = 255
+            localMin = 999999
             for k in range(0, 3):
                 if img.item((i, j, k)) < localMin:
                     localMin = img.item((i, j, k))
@@ -190,10 +175,10 @@ def getMinChannel(img):
 
 
 
-θ = 0.2 #θ is a parameter controlling the selection of the bright parts in the red channel.
+θ = 0.3 #θ is a parameter controlling the selection of the bright parts in the red channel.
 σ = 0.3  #σ is a parameter of a two-dimensional Gaussian function 
 k = 0.5  #k值暂定
-N = 15 #N*N的大小
+N = 49 #N*N的大小
 
 img = cv2.imread(r'C:\Users\Owen\Pictures\HCF-test.jpg')
 #cv_show('original',img)
@@ -214,29 +199,29 @@ L1_normalized = cv2.normalize(original,dst = None,alpha = 0,beta = 1 , norm_type
 
 HCFB,HCFG,HCFR = mat_mean(L1_normalized,θ,N) #各通道均值
 
+
+
 HCFL = L_mean(L,N)  #L均值
 
 HCF = cv2.merge([HCFB,HCFG,HCFR])
 
-HCF = HCF * 255
-HCF = np.uint8(HCF)
-HCF = cv2.GaussianBlur(HCF,(N,N),σ)
-cv_show('HCF',HCF)
-
-
-print(HCFB[int(HCFB.shape[0]/2),int(HCFB.shape[1]/2)])
+HCF_255 = HCF * 255
+HCF_255= np.uint8(HCF_255)
+HCF_255= cv2.GaussianBlur(HCF_255,(N,N),σ)
+cv_show('HCF',HCF_255)
 
 
 #with the HC feedbacks,the cone signals become:
-CSR = R / zero_to_one(HCF[:,:,0])  
-CSG = G / zero_to_one(HCF[:,:,1])
-CSB = B / zero_to_one(HCF[:,:,2])
+CSR = R / HCF[:,:,0]    #存在问题！ HCF含有0  #(4)
+CSG = G / HCF[:,:,1]
+CSB = B / HCF[:,:,2]
 CSL = L / HCFL
+
 
 RGB_merge('CS IMG',CSR,CSG,CSB)
 
 #use the sigmoid function to suppress the dim part of cone signal:
-CSR = CSR - 0.5
+CSR = CSR - 0.5  
 CSG = CSG - 0.5
 CSB = CSB - 0.5
 CSL = CSL - 0.5
@@ -253,6 +238,10 @@ RGB_merge('CO IMG',COR,COG,COB)
 #of rod cells after modulation by the rod horizontal cells
 FbcL = COrod 
 FbsL = L_mean(COrod,N)
+
+middel = np.float64(getMinChannel(img)) / np.float64(getMinChannel(HCF))  #(15)
+k = np.mean(middel)   #(17) k要先算
+
 
 #the output of rod bipolar cells:
 Bprod = max_mat(0,np.float32(cv2.GaussianBlur(FbcL,(5,5),sigmaX=σ) - k * cv2.GaussianBlur(FbsL,(5,5),sigmaX=3*σ)))
@@ -276,24 +265,69 @@ FbsB = L_mean(COB,N)
 #the signals along the OFF pathway fbc and fbs can  be simply obtained by:
 #(12)
 
-ones_mat = np.ones(shape = FbcR.shape,dtype=np.uint8)
+ones_mat = np.ones(shape = FbcR.shape,dtype=np.float64)
 
-FbcR_1 = ones_mat - FbcR #(12)
-FbcG_1 = ones_mat - FbcG
-FbcB_1 = ones_mat - FbcB
+FbcR_1 = np.zeros(shape = FbcR.shape,dtype=np.float64)
+FbcG_1 = np.zeros(shape = FbcR.shape,dtype=np.float64)
+FbcB_1 = np.zeros(shape = FbcR.shape,dtype=np.float64)
+FbsR_1 = np.zeros(shape = FbcR.shape,dtype=np.float64)
+FbsG_1 = np.zeros(shape = FbcR.shape,dtype=np.float64)
+FbsB_1 = np.zeros(shape = FbcR.shape,dtype=np.float64)
+
+FbcR_1 = ones_mat - np.float64(FbcR) #(12)
+FbcG_1 = ones_mat - np.float64(FbcG)
+FbcB_1 = ones_mat - np.float64(FbcB)
+
+FbsR_1 = ones_mat - np.float64(FbsR)
+FbsG_1 = ones_mat - np.float64(FbsG)
+FbsB_1 = ones_mat - np.float64(FbsB)
+
 
 #(7)
-BpR = max_mat(0,np.float32(cv2.GaussianBlur(FbcR,(5,5),sigmaX=σ) - k * cv2.GaussianBlur(FbsR,(5,5),sigmaX = 3 * σ)))
-BpB = max_mat(0,np.float32(cv2.GaussianBlur(FbcB,(5,5),sigmaX=σ) - k * cv2.GaussianBlur(FbsB,(5,5),sigmaX = 3 * σ)))
-BpG = max_mat(0,np.float32(cv2.GaussianBlur(FbcG,(5,5),sigmaX=σ) - k * cv2.GaussianBlur(FbsG,(5,5),sigmaX = 3 * σ)))
+BpR = max_mat(0,np.float64(cv2.GaussianBlur(FbcR,(5,5),sigmaX=σ) - k * cv2.GaussianBlur(FbsR,(5,5),sigmaX = 3 * σ)))
+BpB = max_mat(0,np.float64(cv2.GaussianBlur(FbcB,(5,5),sigmaX=σ) - k * cv2.GaussianBlur(FbsB,(5,5),sigmaX = 3 * σ)))
+BpG = max_mat(0,np.float64(cv2.GaussianBlur(FbcG,(5,5),sigmaX=σ) - k * cv2.GaussianBlur(FbsG,(5,5),sigmaX = 3 * σ)))
 
-BpR_ = max_mat(0,np.float32(cv2.GaussianBlur(FbcR_1,(5,5),sigmaX=σ) - k*cv2.GaussianBlur(FbsR_1,(5*5),sigmaX=3 * σ))) 
-BpB_ = max_mat(0,np.float32(cv2.GaussianBlur(FbcR_1,(5,5),sigmaX=σ) - k*cv2.GaussianBlur(FbsR_1,(5*5),sigmaX=3 * σ)))
-BPG_ = max_mat(0,np.float32(cv2.GaussianBlur(FbcG,(5,5),sigmaX=σ) - k * cv2.GaussianBlur(FbsG,(5,5),sigmaX = 3 * σ)))
+BpR_ = max_mat(0,np.float64(cv2.GaussianBlur(FbcR_1,(5,5),sigmaX=σ) - k*cv2.GaussianBlur(FbsR_1,(5,5),sigmaX = 3 * σ))) 
+BpB_ = max_mat(0,np.float64(cv2.GaussianBlur(FbcR_1,(5,5),sigmaX=σ) - k*cv2.GaussianBlur(FbsR_1,(5,5),sigmaX = 3 * σ)))
+BPG_ = max_mat(0,np.float64(cv2.GaussianBlur(FbcG_1,(5,5),sigmaX=σ) - k*cv2.GaussianBlur(FbsG_1,(5,5),sigmaX = 3 * σ)))
+
 #(13)
-
 Bpy = (BpR + BpB) / 2
 Bpy_ = (BpR_ + BpB_) / 2
+
+
+#(15)
+
+t = np.float64(ones_mat) - getDarkChannel(middel,7)   #求t值 
+cv_show('t',t)
+
+
+
+Bp = cv2.merge([BpR,BpG,BpB])
+S = 3 * getMinChannel(Bp)/ (BpR+BpG+BpB)  #？？ 存在问题，BpR/G/B中含有0
+ #？？ 存在问题，BpR/G/B中含有0  #HSV色彩空间，S值
+                                                                        #逻辑值索引，在不是0的地方才改变其的值
+
+#m的值与双极单元输出的饱和程度成反比。饱和度越低，直观地表明m值越高，因此周围RF对中心RF的贡献越大，有助于增强水下图像的色彩对比度。
+m = np.mean(S) 
+S = ones_mat - S  #图像的饱和度可以作为图像颜色对比度的简单测量
+
+fgc = BpR
+fgs = BpG
+
+fgc_ = BpR_
+fgs_ = BPG_
+
+Gg = max_mat(0,cv2.GaussianBlur(fgc,(N,N),sigmaX=σ)+ m * (cv2.GaussianBlur(fgc,(N,N),sigmaX=σ)-cv2.GaussianBlur(fgs,(N,N),sigmaX= 3 * σ)))
+Gg_ = max_mat(0,cv2.GaussianBlur(fgc_,(N,N),sigmaX=σ)+ m * (cv2.GaussianBlur(fgc_,(N,N),sigmaX=σ)-cv2.GaussianBlur(fgs_,(N,N),sigmaX=3 * σ)))
+
+ 
+F_G = 1 / (1 + np.exp(-10*(Gg-0.5)))
+Won = F_G / (Gg + F_G)
+Woff = ones_mat - Won
+
+output = Won * Gg + Woff * Gg_
 
 
 
